fbdf5369ba8268efe8894b4c1bdac6c66efc925b
kristjan.junus <kristjan.junus@helmes.com>
2024-01-17 13:26:14 +0200

    Refactor code

---
+++ src/app/shared/util/session-storage.util.ts
@@ -0,0 +1,11 @@
+export default class SessionStorageUtil {
+  private static readonly TOKEN_KEY = "checkpoint-token";
+
+  static setAuthToken(value: string): void {
+    sessionStorage.setItem(this.TOKEN_KEY, value)
+  }
+
+  static getAuthToken(): string {
+    return sessionStorage.getItem(this.TOKEN_KEY)
+  }
+}
