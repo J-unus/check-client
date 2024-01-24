a80db888ba889eb6dcc624508384e3da9943f062
kristjan.junus <kristjan.junus@helmes.com>
2024-01-24 10:52:22 +0200

    Add time to chat messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WebSocketService } from './web-socket/web-socket.service';
@@ -13,6 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NotificationInterceptor } from './core/interceptor/notification.interceptor';
 import { MatButtonModule } from '@angular/material/button';
+import { registerLocaleData } from '@angular/common';
+import localeEt from '@angular/common/locales/et';
+import localeEtExtra from '@angular/common/locales/extra/et';
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -38,6 +41,7 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
     MatButtonModule,
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'et' },
     { provide: HTTP_INTERCEPTORS, useClass: NotificationInterceptor, multi: true },
     {
       provide: WebSocketService,
@@ -47,3 +51,11 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+function changeLocaleDateFormat(): void {
+  const DATE = 10;
+  const SHORT_DATE = 0;
+  localeEt[DATE][SHORT_DATE] = 'dd.MM.yyyy';
+}
+changeLocaleDateFormat();
+registerLocaleData(localeEt, 'et', localeEtExtra);
--- src/app/chat-room/dto/message.dto.ts
+++ src/app/chat-room/dto/message.dto.ts
@@ -1,5 +1,6 @@
 export class MessageDto {
   public id: string;
+  public createdAt: string;
   public body: string;
   public sentMessage = false;
 }
